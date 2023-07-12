d75e9aec2143fb42ad9ccff763fd192237a488e8
GowthamMReddy <117168448+GowthamMReddy@users.noreply.github.com>
2023-07-12 14:11:18 +0100

    Created login and signup html pages

--- webpage/__init__.py
+++ webpage/__init__.py
@@ -2,9 +2,12 @@ from flask import Flask, url_for
 
 def create_app():
     app=Flask(__name__)
+    app.config['SECRET_KEY']='tetsinshajshkks'
 
-    @app.route('/')
-    def helloworld():
-        return "<p>Hello, World!</p>"
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     return app
